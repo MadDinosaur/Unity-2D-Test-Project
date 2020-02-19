94302a50134afb57a7c7ff0142ab499de1461c5f
BarbaraDP <1191507@isep.ipp.pt>
2020-02-19 15:17:41 +0000

    create Fall.cs and Player.cs

---
+++ Assets/Scripts/Fall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    public GameObject player;
+    private Vector2 respawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPoint = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("He ded x_x");
+        player.transform.position = respawnPoint;
+
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    private float movement = 0f;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, jumpSpeed);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+    }
+}
