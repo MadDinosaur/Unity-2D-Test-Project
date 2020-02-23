47ef68a757ca276de1a817c99bde3351543a272d
Educorreia932 <skelozard1@gmail.com>
2020-02-23 16:38:23 +0000

    Started to develop projectiles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     private Rigidbody2D rigidBody;
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
@@ -15,24 +14,19 @@ public class Player : MonoBehaviour
     public LayerMask groundLayer;
     private bool isTouchingGround;
 
-    void Start()
-    {
+    void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
         playerAnimation = GetComponent<Animator>();
-
     }
 
-    void Update()
-    {
+    void Update() {
         movement = Input.GetAxis("Horizontal");
         isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+
         if (Input.GetButtonDown("Jump") && isTouchingGround)
-        {
             rigidBody.velocity = new Vector2(movement * speed, jumpSpeed);
-        }
+        
         else
-        {
             rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
-        }
     }
 }
