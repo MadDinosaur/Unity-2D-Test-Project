832ee47432b8da23bcbc836c3438db83c4979b69
Educorreia932 <skelozard1@gmail.com>
2020-02-23 19:29:50 +0000

    Fixed bug where bullets would fly in the wrong direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     private Rigidbody2D rigidBody;
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
@@ -15,36 +14,35 @@ public class Player : MonoBehaviour
     public float groundCheckRadius;
     public LayerMask groundLayer;
     private bool isTouchingGround;
+    private bool isFacingRight;
 
-    void Start()
-    {
+    void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
         playerAnimation = GetComponent<Animator>();
-
     }
 
-    void Update()
-    {
+    void Update() {
         movement = Input.GetAxis("Horizontal");
         isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+
         if (Input.GetButtonDown("Jump") && isTouchingGround)
-        {
             rigidBody.velocity = new Vector2(movement * speed, jumpSpeed);
-        }
+
         else
-        {
             rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
-        }
 
         //Animation
         playerAnimation.SetFloat("Speed", Math.Abs(rigidBody.velocity.x));
         playerAnimation.SetBool("isTouchingGround", isTouchingGround);
-        if (movement >= 0f)
-        {
-            transform.localScale = new Vector2(-Math.Abs(rigidBody.transform.localScale.x), rigidBody.transform.localScale.y);
-        } else
-        {
-            transform.localScale = new Vector2(Math.Abs(rigidBody.transform.localScale.x), rigidBody.transform.localScale.y);
+
+        if (movement > 0f && !isFacingRight) {
+            isFacingRight = true;
+            transform.Rotate(0f, 180f, 0f);
+        }
+
+        else if (movement < 0f && isFacingRight) {
+            isFacingRight = false;
+            transform.Rotate(0f, 180f, 0f);
         }
     }
 }
