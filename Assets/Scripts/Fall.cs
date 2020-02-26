2c3c5617cc9dd74a23cb940b4ded3c76192c871f
BarbaraDP <1191507@isep.ipp.pt>
2020-02-26 16:16:08 +0000

    Added death, attack and jumping animation. Updated walking animation.

--- Assets/Scripts/Fall.cs
+++ Assets/Scripts/Fall.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Fall : MonoBehaviour
 {
     public GameObject player;
+    Animator playerAnimation;
     private Vector2 respawnPoint;
+    private bool triggerRespawn;
     // Start is called before the first frame update
     void Start()
     {
+        playerAnimation = player.GetComponent<Animator>(); 
         respawnPoint = player.transform.position;
     }
 
@@ -16,13 +19,16 @@ public class Fall : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        playerAnimation.SetBool("triggerRespawn", triggerRespawn);
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("He ded x_x");
         player.transform.position = respawnPoint;
-
+        triggerRespawn = true;
         player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        triggerRespawn = false;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,25 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    //Movement
     private Rigidbody2D rigidBody;
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    private float movement = 0f;
-    private Animator playerAnimation;
-
+    public float speed;
+    public float jumpSpeed;
+    private float movement;
     public Transform groundCheckPoint;
     public float groundCheckRadius;
     public LayerMask groundLayer;
     private bool isTouchingGround;
-    private bool isFacingRight;
+    private bool isFacingRight = true;
+    //Animation
+    private Animator playerAnimation;
 
-    void Start() {
+    void Start()
+    {
         rigidBody = GetComponent<Rigidbody2D>();
         playerAnimation = GetComponent<Animator>();
+        transform.Rotate(0f, 180f, 0f);
     }
 
-    void Update() {
+    void Update()
+    {
+        //Movement
         movement = Input.GetAxis("Horizontal");
         isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
 
@@ -31,18 +37,21 @@ public class Player : MonoBehaviour {
         else
             rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
 
-        //Animation
-        playerAnimation.SetFloat("Speed", Math.Abs(rigidBody.velocity.x));
-        playerAnimation.SetBool("isTouchingGround", isTouchingGround);
-
-        if (movement > 0f && !isFacingRight) {
+        if (movement > 0f && !isFacingRight)
+        {
             isFacingRight = true;
             transform.Rotate(0f, 180f, 0f);
         }
 
-        else if (movement < 0f && isFacingRight) {
+        else if (movement < 0f && isFacingRight)
+        {
             isFacingRight = false;
             transform.Rotate(0f, 180f, 0f);
         }
+
+        //Animation
+        playerAnimation.SetFloat("Speed", Math.Abs(rigidBody.velocity.x));
+        playerAnimation.SetBool("isTouchingGround", isTouchingGround);
+        
     }
 }
